8d2348b27255ec79cf8591289840eccda0ffbed5
Ivan <ipostu20000127@gmail.com>
2020-02-25 01:34:04 -0500

    initial commit

---
+++ src/main/java/com/app/password_manager/Main.java
@@ -0,0 +1,11 @@
+package com.app.password_manager;
+
+import com.app.password_manager.ui.MainWindow;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new MainWindow();
+    }
+
+}
---
+++ src/main/java/com/app/password_manager/ui/MainWindow.java
@@ -0,0 +1,25 @@
+package com.app.password_manager.ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame {
+
+
+
+
+    public MainWindow(){
+        super();
+
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Password Manager");
+        setSize(new Dimension(800, 600));
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setLayout(null);
+
+        setVisible(true);
+    }
+
+}
